d9990a8cdef3d4fb68255f32bdf97a58c51e14be
Ha Goeun <sera1193@naver.com>
2023-01-27 23:46:33 +0900

    Add OrderServiceTest

---
+++ src/test/java/springjpasideproject/cakebake/domain/service/OrderServiceTest.java
@@ -0,0 +1,111 @@
+package springjpasideproject.cakebake.domain.service;
+
+
+import jakarta.persistence.EntityManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import springjpasideproject.cakebake.domain.Member;
+import springjpasideproject.cakebake.domain.Order;
+import springjpasideproject.cakebake.domain.OrderStatus;
+import springjpasideproject.cakebake.domain.Product;
+import springjpasideproject.cakebake.domain.repository.OrderRepository;
+import springjpasideproject.cakebake.exception.NotEnoughStockException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired EntityManager em;
+    @Autowired OrderService orderService;
+    @Autowired OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+
+        //given
+        Member member = createMember();
+        Product product = createProduct("블루베리 케이크", "블루베리", "url", 20000, 10);
+
+        int orderCount = 2;
+
+        //when
+        Long orderId = orderService.order(member.getId(), product.getId(), orderCount, "Kim", "010-1234-5678", "hi@naver.com", "빠른 배송");
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderProducts().size());
+        assertEquals("주문 가격은 가격 * 수량이다.", 20000 * orderCount, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, product.getStockQuantity());
+
+}
+
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() throws Exception {
+
+        //given
+        Member member = createMember();
+        Product product = createProduct("블루베리 케이크", "블루베리", "url", 20000, 10);
+
+        int orderCount = 11;
+
+        //when
+        orderService.order(member.getId(), product.getId(), orderCount, "Kim", "010-1234-5678", "hi@naver.com", "빠른 배송");
+
+        //then
+        fail("재고 수량 부족 예외가 발행해야 한다.");
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+
+        //given
+        Member member = createMember();
+        Product product = createProduct("블루베리 케이크", "블루베리", "url", 20000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), product.getId(), orderCount, "Kim", "010-1234-5678", "hi@naver.com", "빠른 배송");
+
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("주문 취소시 상태는 CANCEL이다.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문 취소시 상태는 CANCEL이다.", 10, product.getStockQuantity());
+    }
+
+    private Product createProduct(String name, String ingredient, String image, int price, int stockQuantity) {
+        Product product = new Product();
+        product.setName(name);
+        product.setIngredient(ingredient);
+        product.setImage(image);
+        product.setPrice(price);
+        product.setStockQuantity(stockQuantity);
+        em.persist(product);
+        return product;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setUserId("hi1234");
+        member.setName("Kim");
+        member.setPassword("j");
+        member.setPhone("010-0000-0000");
+        member.setEmail("todnxnlrla@naver.com");
+        em.persist(member);
+        return member;
+    }
+
+
+}
